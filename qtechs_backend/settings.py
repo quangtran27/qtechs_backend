153aae7b3ff3b2aba431854734bdd1c6fc2a17e7
Quang Tran <quangtv2702@gmail.com>
2023-05-27 00:17:43 +0700

    Delete VSCode setting, change to use PostgreSQL database

--- qtechs_backend/settings.py
+++ qtechs_backend/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'qtechs_backend.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+		'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'qtechs',
+    'USER': 'qtechs',
+    'PASSWORD': 'qtechs',
 	}
 }
 

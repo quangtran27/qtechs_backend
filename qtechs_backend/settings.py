2572ed6c6e9fe40904255020425352b60c887c0b
Quang Tran <quangtv2702@gmail.com>
2023-05-28 00:17:26 +0700

    update cors allowed origins

--- qtechs_backend/settings.py
+++ qtechs_backend/settings.py
@@ -138,7 +138,11 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+  'https://qtechs-frontend.vercel.app/',
+)
 
 
 REST_FRAMEWORK = {

95ba0faac8b46f4c44b2f70d5938b09973b11fd8
Quang Tran <quangtv2702@gmail.com>
2023-05-27 16:20:46 +0700

    Update for static

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
-# build_files.sh
-pip install -r requirements.txt
-
-# make migrations
-python manage.py makemigrations
-python manage.py migrate
-python3.9 manage.py collectstatic
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"

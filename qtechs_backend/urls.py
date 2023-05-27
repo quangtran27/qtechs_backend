70e72d74c962f80e1ab89f30da179e77cbb2ac5d
Quang Tran <quangtv2702@gmail.com>
2023-05-27 16:54:56 +0700

    Update collect static

--- qtechs_backend/urls.py
+++ qtechs_backend/urls.py
@@ -2,17 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
 
 from users.views import login
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/auth/login', login, name='login'),
-    path("api/", include("products.urls")),
-    path("api/promotions/", include("promotions.urls")),
-    path("api/reviews/", include("reviews.urls")),
-    path("api/users/", include("users.urls")),
-    path("api/carts/", include("carts.urls")),
+    path('api/', include('products.urls')),
+    path('api/promotions/', include('promotions.urls')),
+    path('api/reviews/', include('reviews.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/carts/', include('carts.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

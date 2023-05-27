a6e74295931238208ae657e965b3dec4aa795f65
Quang Tran <quangtv2702@gmail.com>
2023-05-27 23:12:18 +0700

    update media url

--- qtechs_backend/settings.py
+++ qtechs_backend/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/api/media/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))

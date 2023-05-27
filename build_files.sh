3dba9566ca9b05998ee4670ec14cfbe6ef11482d
Quang Tran <quangtv2702@gmail.com>
2023-05-27 15:18:07 +0700

    Update build_sh `python39` -> `python3.9`

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,6 @@
 pip install -r requirements.txt
 
 # make migrations
-python39 manage.py makemigrations
-python39 manage.py migrate 
-python39 manage.py collectstatic
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
